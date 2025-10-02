881e4b73d13019eb49636216e5ff2991a2b62b12
Alex A. Yermoshenko <alex.a.ermoshenko@gmail.com>
2025-10-02 23:28:10 +0300

    Adds AMD-optimized OpenCL backend

--- compile.bat
+++ compile.bat
@@ -1,19 +1,12 @@
 @rem setupt windows var
 call vcvars64.bat
 
-@echo %1 --machine 64 or 32
-@echo %2  -G 
-
-set machine_num=%1%
-set debug_opt=%2%
-
-if "%machine_num%" == "" set machine_num=64
-
 @echo Execute NVCC...
 
-@echo nvcc -Xcompiler "/wd 4819" -I"./" -use_fast_math -ftz=true -prec-div=false -prec-sqrt=false -arch=compute_75 -O3 --machine %machine_num% %debug_opt% -ptx -o clguetzli/clguetzli.cu.ptx%machine_num%  clguetzli\clguetzli.cu
-nvcc -Xcompiler "/wd 4819" -I"./" -use_fast_math -ftz=true -prec-div=false -prec-sqrt=false -arch=compute_75 -O3 --machine %machine_num% %debug_opt% -ptx -o clguetzli/clguetzli.cu.ptx%machine_num%  clguetzli\clguetzli.cu
+@echo nvcc -Xcompiler "/wd 4819" -I"./" -use_fast_math -arch=compute_75 -O3 --machine 64 -G -ptx -o clguetzli/clguetzli.cu.ptx  clguetzli\clguetzli.cu
+nvcc -Xcompiler "/wd 4819" -I"./" -use_fast_math -arch=compute_75 -O3 --machine 64 -G -ptx -o clguetzli/clguetzli.cu.ptx clguetzli\clguetzli.cu
 
-python format_header.py clguetzli/clguetzli.cu.ptx%machine_num% clguetzli/clguetzli_cu_ptx.h clguetzli_cu%machine_num%
+
+python format_header.py clguetzli/clguetzli.cu.ptx clguetzli/clguetzli_cu_ptx.h clguetzli_cu64
 python format_header.py clguetzli/clguetzli.cl clguetzli/clguetzli_cl_src.h clguetzli_cl_src
 python format_header.py clguetzli/clguetzli_amd.cl clguetzli/clguetzli_cl_amd_src.h clguetzli_cl_amd_src
